c64cc72692bbbd47c1c7050676294fa25dfe8cd7
Affiq <affiqrbk@gmail.com>
2023-03-22 19:27:55 +0000

    Upload images

--- GestureRecognition/main.py
+++ GestureRecognition/main.py
@@ -12,14 +12,17 @@ import SettingsComponents.SliderBar as sb
 
 # Define a new settings container to hold the components
 settings_container = sc.SettingsContainer()
+
 # Define some components to add to the settings menu
 volume = sb.SliderBar("Volume", 50)
 brightness = sb.SliderBar("Brightness", 20)
 show_hands = tc.ToggleComponent("Show Hands", "Show", "Hide", True)
+rgb = sb.SliderBar("RGB", 10)
 # Add the components to the settings container
 settings_container.add_component(volume)
 settings_container.add_component(brightness)
 settings_container.add_component(show_hands)
+settings_container.add_component(rgb)
 
 # Define new settings module, and assign settings container as one of the attributes
 new_settings = SettingsModule.SettingsModule()
